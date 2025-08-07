fdad6ccbe02bc3ab1a1a1d8bf0497e044cafb66b
Vincent991105 <qwer012268331290@gmail.com>
2025-08-07 14:17:05 +0800

    【delete】刪除不需要的資料

--- backend/bridge/web/api/serializer/test_serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from web.models import TestItem
-
-class TestItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TestItem
-        fields = ['id', 'name', 'description']
--- backend/bridge/web/api/urls.py
+++
@@ -1,7 +0,0 @@
-# from . import views
-from django.urls import path
-from .view.project_list_view import ProjectListView
-
-urlpatterns = [
-    path("projects/", ProjectListView.as_view(), name="project-list"),
-] 
